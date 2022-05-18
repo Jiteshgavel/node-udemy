c2b214d77e5f26eb1bb731109faa31aa5e8bcb2f
jiteshgavel <jiteshgavel000@gmail.com>
2022-05-18 20:48:28 +0530

    route basic complite

--- node basics/app.js
+++ node basics/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const routes =require('./routes.js')
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes.handler);
+const server = http.createServer(app);
 
 server.listen(3000);
