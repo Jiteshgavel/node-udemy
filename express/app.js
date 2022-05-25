7c8fa33a1ef685864cb3da336a55e94543b33167
jiteshgavel <jiteshgavel000@gmail.com>
2022-05-25 23:21:40 +0530

    handlebars tamplete engine basic complete

--- express/app.js
+++ express/app.js
@@ -1,22 +1,33 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 const app = express();
 
-app.set('view engine','pug');
-app.set('views','view');
-const adminRoutes = require('./routes/admin');
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/admin', adminRoutes.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use(express.static(path.join(__dirname,'public')))
-app.use((req,res,next)=>{
- 
-   res.status(404).render('404',{ pageTitle : 'Page not found'});
-})
-
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
