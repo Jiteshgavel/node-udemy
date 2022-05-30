066d85c0181f443c545292d486bd0ae1cbd80e22
jiteshgavel <jiteshgavel000@gmail.com>
2022-05-30 22:03:01 +0530

    section 8  optional complete

--- express/controllers/products.js
+++ express/controllers/products.js
@@ -1,7 +1,7 @@
 const ProductModel = require('../models/product')
 
 exports.getAddProduct=(req, res, next) => {
-    res.render('add-product', { pageTitle: 'Add Product',
+    res.render('admin/add-product', { pageTitle: 'Add Product',
      path: '/admin/add-product', 
      formsCSS: true,
       productCSS: true, 
@@ -17,7 +17,7 @@ exports.postAddProduct=(req, res, next)=>{
 
 exports.getProducts = (req, res, next) => {
     ProductModel.fetchAll((products)=>{
-        res.render('shop', {
+        res.render('shop/product-list', {
             prods: products,
             pageTitle: 'Shop',
             path: '/',
